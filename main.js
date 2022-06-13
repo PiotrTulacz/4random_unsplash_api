7a6d22d1385816042ad32b32508b0ee96bb5ffa2
Piotrek <tulaczp18@gmail.com>
2022-06-13 12:28:28 +0200

    final commit

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const endpoint = `https://api.unsplash.com/photos/random/?client_id=49J6Wp8HyiSvGTvwD-YCQRRoBhLd6H_oj1IGFYvCKWE&count=4`;
 
 let img = document.querySelectorAll(".change");
-let description = document.querySelectorAll(".description");
+let descriptions = document.querySelectorAll(".description");
 let author = document.querySelectorAll(".author");
 let date = document.querySelectorAll(".date");
 
@@ -14,7 +14,9 @@ fetch(endpoint)
         for(i=0; i<4;i++){
             img[i].src = jsonData[i].urls.small;
             author[i].innerText = jsonData[i].user.name;
-            description[i].innerText = jsonData[i].description;
+            let description = jsonData[i].description;
+
+            descriptions[i].innerText = description;
             let cutdata = jsonData[i].created_at.substring(0, 10);
             date[i].innerText = cutdata.replace("-", "/");
         }
